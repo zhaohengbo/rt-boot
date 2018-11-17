4f02d3d529dc3794ddd23b87fb161944e067d1f0
zhaohengbo <1755923166@qq.com>
2018-11-18 00:01:54 +0800

    change some build step

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ export MAKECMD="make --silent ARCH=mips CROSS_COMPILE=mips-openwrt-linux-"
 export PATH=$BUILD_TOPDIR/../openwrt-sdk-18.06.1-ar71xx-generic_gcc-7.3.0_musl.Linux-x86_64/staging_dir/toolchain-mips_24kc_gcc-7.3.0_musl/bin:$PATH
 
 make clean
-make tp-link_tl-wr740n_v4
+make tp-link_tl-wr810n_v2
 
